010fe40e5e9f442303357e438a8ed8f9795d5dad
EmiSem <just_dragon29@mail.ru>
2025-05-15 12:43:01 +0300

    Созданы карточка термина, список терминов с пробрасыванием ппропов, добавлена стилизация

---
+++ src/TermList.jsx
@@ -0,0 +1,14 @@
+import { TermCard } from './TermCard';
+import './TermList.scss';
+
+export const TermList = ({ terms }) => {
+    return <ul className="term-list">
+        {
+            terms.map((item) => (
+                <li className="term-list__item" key={item.id}>
+                    <TermCard title={item.title} description={item.description} />
+                </li>
+            ))
+        }
+    </ul>
+}
